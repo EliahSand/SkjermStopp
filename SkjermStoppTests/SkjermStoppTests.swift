96173ec3aa4127612469d4468b23369e9bbf7de3
Eliah Sand <eliahsand@outlook.com>
2025-09-04 13:20:47 +0200

    Initial Commit

---
+++ SkjermStoppTests/SkjermStoppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkjermStoppTests.swift
+//  SkjermStoppTests
+//
+//  Created by Eliah Sand on 04/09/2025.
+//
+
+import Testing
+@testable import SkjermStopp
+
+struct SkjermStoppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

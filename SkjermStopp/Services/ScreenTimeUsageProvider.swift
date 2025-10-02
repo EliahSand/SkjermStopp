0324cf10d90077922b892d70a200b746312ef594
Eliah Sand <eliahsand@outlook.com>
2025-10-02 11:49:12 +0200

    details

--- SkjermStopp/Services/ScreenTimeUsageProvider.swift
+++ SkjermStopp/Services/ScreenTimeUsageProvider.swift
@@ -14,6 +14,34 @@ struct ScreenTimeUsageProvider: UsageProvider {
         // start of today 00:00
         let startComponent = DateComponents(hour: 0, minute: 0)
         let endComponent = Calendar.current.dateComponents([.hour, .minute], from: Date())
+        
+        let center = DeviceActivityCenter()
+        let report: DeviceActivityReport = try await center.activity(
+                    for: DeviceActivityEvent.Name("daily"),
+                    from: startComponent,
+                    to: endComponent
+                )
+
+                let totalActivity: TimeInterval = report.categories.values.reduce(0) { partial, data in
+                    partial + data.totalActivityDuration
+                }
+
+            return totalActivity
+        
+        var totalDuration: TimeInterval = 0
+
+                for appReport in report.pplications.values {
+                    totalDuration += appReport.totalActivityDuration
+                }
+
+                for categoryReport in report.categories.values {
+                    totalDuration += categoryReport.totalActivityDuration
+                }
+
+            return totalDuration
+        
+        /*
+        gammel versjon
         let schedule = DeviceActivitySchedule(intervalStart: startComponent,
                                                 intervalEnd: endComponent,
                                                 repeats: false)
@@ -25,6 +53,7 @@ struct ScreenTimeUsageProvider: UsageProvider {
                     to: endComponent
                 )
         return report.totalActivityDuration
+        */
 #else
         throw NSError(domain: "ScreenTimeUnavailable", code: 0)
 #endif
